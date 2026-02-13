534ec8ff317acbfad8ce45fddbda96d5998a8834
Maxime Bokobza <max@codehawks.eu>
2026-02-13 22:07:00 +0100

    Initial commit

---
+++ ImageBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageBrowser
+//
+//  Created by Maxime Bokobza on 13/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageBrowser/ImageBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageBrowserApp.swift
+//  ImageBrowser
+//
+//  Created by Maxime Bokobza on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ImageBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
