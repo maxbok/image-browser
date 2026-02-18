1ff6e340c7b82cc649d5848e5a45776e0a7bbdac
Maxime Bokobza <max@codehawks.eu>
2026-02-18 14:43:24 +0100

    Add Testing configuration
    To disable durations during tests

--- ImageBrowser/Presentation/DesignSystem/Durations.swift
+++ ImageBrowser/Presentation/DesignSystem/Durations.swift
@@ -9,6 +9,10 @@ import Foundation
 
 extension RunLoop.SchedulerTimeType.Stride {
 
+#if !TESTING
     static let textInputDebounce: Self = .milliseconds(500)
+#else
+    static let textInputDebounce: Self = 0
+#endif
 
 }
